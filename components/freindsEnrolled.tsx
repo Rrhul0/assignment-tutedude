f261f91c56815ab8ca78c5585058c3957885d749
Rrhul0 <rr025765@gmail.com>
2023-06-01 22:35:05 +0530

    added friend enrolled section

---
+++ components/freindsEnrolled.tsx
@@ -0,0 +1,71 @@
+export default function FriendsEnrolled() {
+    return (
+        <section>
+            <h2 className='text-xl font-semibold'>
+                Friends who enrolled <span className='font-normal text-dark'>(3)</span>
+            </h2>
+            <div className='flex  gap-5 overflow-x-scroll pt-5 text-white'>
+                <div className='flex h-56 w-[358px] flex-shrink-0 flex-col justify-between rounded-2xl bg-gradient p-5 '>
+                    <div className='flex flex-col gap-2'>
+                        <div className='flex items-center justify-between'>
+                            <p className='font-bold'>Dhiraj Saxsena</p>
+                            <span className='text-sm'>14 Sep, 2022</span>
+                        </div>
+                        <p className='text-sm font-medium'>Courses Enrolled(6)</p>
+                        <div className='flex flex-wrap gap-[10px] text-sm'>
+                            <div className='rounded-3xl border border-[#EEAEAE] px-2 py-1'>UI/UX</div>
+                            <div className='rounded-3xl border border-[#EEAEAE] px-2 py-1'>Photoshop</div>
+                            <div className='rounded-3xl border border-[#EEAEAE] px-2 py-1'>Illustrator</div>
+                            <div className='rounded-3xl border border-[#EEAEAE] px-2 py-1'>Python</div>
+                            <div className='rounded-3xl border border-[#EEAEAE] px-2 py-1'>MERN</div>
+                            <div className='rounded-3xl border border-[#EEAEAE] px-2 py-1'>Java</div>
+                        </div>
+                    </div>
+                    <p>
+                        Referral Amount <span className='text-2xl'>₹185</span>
+                    </p>
+                </div>
+                <div className='flex h-56 w-[358px] flex-shrink-0 flex-col justify-between rounded-2xl bg-gradient p-5 '>
+                    <div className='flex flex-col gap-2'>
+                        <div className='flex items-center justify-between'>
+                            <p className='font-bold'>Subhash Mishra</p>
+                            <span className='text-sm'>12 Sep, 2022</span>
+                        </div>
+                        <p className='text-sm font-medium'>Courses Enrolled(6)</p>
+                        <div className='flex flex-wrap gap-[10px] text-sm'>
+                            <div className='rounded-3xl border border-[#EEAEAE] px-2 py-1'>UI/UX</div>
+                            <div className='rounded-3xl border border-[#EEAEAE] px-2 py-1'>Photoshop</div>
+                            <div className='rounded-3xl border border-[#EEAEAE] px-2 py-1'>Illustrator</div>
+                            <div className='rounded-3xl border border-[#EEAEAE] px-2 py-1'>Python</div>
+                            <div className='rounded-3xl border border-[#EEAEAE] px-2 py-1'>MERN</div>
+                            <div className='rounded-3xl border border-[#EEAEAE] px-2 py-1'>Java</div>
+                        </div>
+                    </div>
+                    <p>
+                        Referral Amount <span className='text-2xl'>₹185</span>
+                    </p>
+                </div>
+                <div className='flex h-56 w-[358px] flex-shrink-0 flex-col justify-between rounded-2xl bg-gradient p-5 '>
+                    <div className='flex flex-col gap-2'>
+                        <div className='flex items-center justify-between'>
+                            <p className='font-bold'>Prafull Kumar</p>
+                            <span className='text-sm'>10 Sep, 2022</span>
+                        </div>
+                        <p className='text-sm font-medium'>Courses Enrolled(6)</p>
+                        <div className='flex flex-wrap gap-[10px] text-sm'>
+                            <div className='rounded-3xl border border-[#EEAEAE] px-2 py-1'>UI/UX</div>
+                            <div className='rounded-3xl border border-[#EEAEAE] px-2 py-1'>Photoshop</div>
+                            <div className='rounded-3xl border border-[#EEAEAE] px-2 py-1'>Illustrator</div>
+                            <div className='rounded-3xl border border-[#EEAEAE] px-2 py-1'>Python</div>
+                            <div className='rounded-3xl border border-[#EEAEAE] px-2 py-1'>MERN</div>
+                            <div className='rounded-3xl border border-[#EEAEAE] px-2 py-1'>Java</div>
+                        </div>
+                    </div>
+                    <p>
+                        Referral Amount <span className='text-2xl'>₹185</span>
+                    </p>
+                </div>
+            </div>
+        </section>
+    )
+}
